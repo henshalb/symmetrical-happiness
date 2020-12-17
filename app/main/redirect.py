c9e9d504d16108da90e8ca546e10515c59df5b1c
Henshal B <henshalb@gmail.com>
2020-12-17 14:50:12 +0530

    secured

--- app/main/redirect.py
+++ app/main/redirect.py
@@ -1,18 +1,28 @@
-from flask import render_template,request,jsonify,session
+from flask import render_template,request,\
+    jsonify,session,redirect,url_for
+from flask.templating import render_template_string
 from . import main
 from .routes import connection
+from functools import wraps
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not 'USER_LOGGED_IN' in session:
+            return render_template('loginspec.html', next=request.url)
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 @main.route('/q/<username>',methods=['GET'])
+@login_required
 def result(username):
-    if ('USER_LOGGED_IN' in session):
-        cursor = connection.cursor()
-        sqlko = "select doctor_fullname from doctor where doctor_username = %s"
-        datako = (username)
-        connection.ping(reconnect=True)
-        cursor.execute(sqlko, datako)
-        out = cursor.fetchone()
-        name = session['USER_DISPLAY_NAME']
-        return render_template('result.html',doctor_name=out[0],name=name)
-    else:
-        return render_template('loginspec.html')
+    cursor = connection.cursor()
+    sqlko = "select doctor_fullname from doctor where doctor_username = %s"
+    datako = (username)
+    connection.ping(reconnect=True)
+    cursor.execute(sqlko, datako)
+    out = cursor.fetchone()
+    name = session['USER_DISPLAY_NAME']
+    return render_template('result.html',doctor_name=out[0],name=name)
     
